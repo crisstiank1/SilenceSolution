86cb4db1bb49332505bc455525e569492f5f551b
crisstiank1 <cristianpescoi15@gmail.com>
2026-04-23 19:24:53 -0500

    Fix GitHub Pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,27 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   modules: [
     '@nuxtjs/color-mode',
     '@vueuse/motion/nuxt',
     '@nuxt/image'
   ],
+
   vite: {
     plugins: [
       tailwindcss()
     ]
   },
+
   css: ['~/assets/css/app.css'],
+
   colorMode: {
     classSuffix: ''
   },
+
   app: {
+    baseURL: '/SilenceSolution/',
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: 'Silence Solution',
@@ -28,7 +34,7 @@ export default defineNuxtConfig({
         lang: 'es'
       },
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/x-icon', href: '/SilenceSolution/favicon.ico' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;700;900&family=Inter:wght@400;500&display=swap' }
